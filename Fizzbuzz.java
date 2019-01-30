195a18828dcf884b4804b32908d9c364f075b9cf
Gagan <gaganyv8@gmail.com>
2019-01-29 22:16:04 -0500

    FizzBuzz problem

--- Fizzbuzz.java
+++ Fizzbuzz.java
@@ -2,6 +2,13 @@ import java.util.*;
 /* FizzBuzz problem */
 class Fizzbuzz{  
 	
+	/* 
+		f(n) = [ (n%3 and n%5) = 0 => FizzBuzz ]
+		f(n) = [ (n%3) = 0 => Fizz ]
+		f(n) = [ (n%5) = 0 => Buzz ]
+		f(n) = [ (n%3 or n%5) != 0 => number ]
+		
+	*/
 	 static public void fizzbuzz(int n){
 		for(int i=0; i<=n; i++){
 			if(i%3 == 0 && i%5 == 0){
@@ -14,9 +21,11 @@ class Fizzbuzz{
 				System.out.println(i);
 			}
 		}
-	} 
+	}
+	
 	public static void main(String args[]){  
 	 Scanner scan = new Scanner(System.in);
+	 System.out.print("Enter an number: ");
 	 int value = scan.nextInt();
 	 fizzbuzz(value);  
 	}  
