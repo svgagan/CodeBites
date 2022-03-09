adc505fe07afb6bb310352a8b30a0347681c8c82
Gagan Solur Venkatesh <gagan.solurvenkatesh@egen.solutions>
2022-03-09 15:24:13 -0600

    Graphs updates

---
+++ Graphs/Graph.java
@@ -0,0 +1,41 @@
+package Graphs;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class Graph<T> {
+
+    // We use Hashmap to store the edges in the graph as an adjancency list.
+    private Map<T, List<T>> adjVertices = new HashMap<>();
+
+    // This function adds a new vertex to the graph
+    public void addVertex(T vertex) {
+        adjVertices.putIfAbsent(vertex, new LinkedList<T>());
+    }
+
+    // This function adds the edge between source to destination
+    public void addEdges(T sourceVertex, T destinationVertex, boolean isItBiDirectional) {
+        addVertex(sourceVertex);
+        addVertex(destinationVertex);
+
+        adjVertices.get(sourceVertex).add(destinationVertex);
+        if(isItBiDirectional)
+            adjVertices.get(destinationVertex).add(sourceVertex);
+    }
+
+    // Prints the adjancency list of each vertex.
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (T sourceNode : adjVertices.keySet()) {
+            builder.append(sourceNode.toString()).append(": ");
+            for (T destinationNode : adjVertices.get(sourceNode)) {
+                builder.append(destinationNode.toString()).append(" ");
+            }
+            builder.append("\n");
+        }
+        return (builder.toString());
+    }
+}
---
+++ Graphs/PrintGraph.java
@@ -0,0 +1,18 @@
+package Graphs;
+
+public class PrintGraph {
+
+    public static void main(String[] args){
+        Graph<Integer> graph = new Graph<Integer>();
+
+        graph.addEdges(0, 1, true);
+        graph.addEdges(0, 4, true);
+        graph.addEdges(1, 2, true);
+        graph.addEdges(1, 3, true);
+        graph.addEdges(1, 4, true);
+        graph.addEdges(2, 3, true);
+        graph.addEdges(3, 4, true);
+
+        System.out.println("Graph:\n" + graph.toString());
+    }
+}
